33cdd43d13b8ee4c3ca0f16d4c6544eed8fb7a0a
Carmen V Ramos <carmenvramos@yahoo.com>
2019-03-20 08:43:40 -0700

    Added Poll component & snapshots test passing

--- src/reducers/session.js
+++ src/reducers/session.js
@@ -4,12 +4,12 @@ const initialState = {
   token: ''
 };
 
-export default function reducer(State = initialState, action) {
+export default function reducer(state = initialState, action) {
   console.log('action payload', action.payload);
   switch(action.type) {
     case SET_SESSION:
       return action.payload;
     default:
-    return state;  
+      return state;  
   }
 }
