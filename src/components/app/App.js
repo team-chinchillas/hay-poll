3bc3bd80605b94a0bb64b9ad452fa92896c9854f
Kate Dameron <hello@katedameron.com>
2019-03-19 17:14:40 -0700

    merge (#5)
    * signin route to be handled by auth0 (#1)
    
    * Added session reducer (#2)
    
    * Added Polls component (#3)
    
    * Added Polls component
    
    * Polls snapshots test passing
    
    * Auth0 (#4)
    
    * signin route to be handled by auth0
    
    * service/request.js
    
    * placeholder poll reducer plus combined reducer
    
    * session selector
    
    * Callback HOC
    
    * session.js in actions
    
    * services/auth.js set up to console log the user returned
    
    * typo in auth.js
    
    * refactor session reducer
    
    * updated typo in combineReducers
    
    * auth0 working on loading page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import 'normalize.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Loading from '../Loading';
+import { withSession } from '../../containers/auth/withSession';
+import Callback from '../../containers/auth/Callback';
+import Home from '../Home';
 
 export default function App() {
   return (
-    <h1>h1</h1>
+    <Router>
+      <>
+      <Switch>
+        <Route path="/callback" component={Callback}></Route>
+        <Route path="/home" component={Home}></Route>
+        <Route path="/" component={withSession(Loading)}></Route>
+      </Switch>
+      </>
+    </Router>
   );
 }
